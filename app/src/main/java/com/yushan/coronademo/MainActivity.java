a454738c271b52412168832c56a8b7b109c27df0
lixz <lixz@wanbu.com.cn>
2017-07-14 11:24:32 +0800

    first updata

--- app/src/main/java/com/yushan/coronademo/MainActivity.java
+++ app/src/main/java/com/yushan/coronademo/MainActivity.java
@@ -14,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
+    }
+
+    private void initView(){
         corona = (CoronaView) findViewById(R.id.corona);
         corona.setViewOnClickListener(new CoronaView.ViewOnClickListener() {
             @Override
@@ -22,5 +26,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 }
